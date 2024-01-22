19a123b11cfd529b086910a9a967e4c0357dafa8
Ekhion76 <76602492+Ekhion76@users.noreply.github.com>
2024-01-22 14:51:10 +0100

    Add offline labor timestamp

--- server/labor.lua
+++ server/labor.lua
@@ -1,17 +1,13 @@
 --- @param playerId number (source)
 --- @return number, nil |boolean, string success and, in case of an error, the reason as well
 function getLabor(playerId)
-
     if not Config.systemMode.labor then
-
         return false, 'the_system_is_turned_off'
     end
 
     if not tonumber(playerId) or not ECO.meta[playerId] then
-
         return false, 'not_found_metadata'
     end
-
     return ECO.meta[playerId].labor.val
 end
 
@@ -19,19 +15,17 @@ end
 --- @param amount number of labor points
 --- @return boolean success and, in case of an error, the reason as well
 function setLabor(playerId, amount)
+    amount = tonumber(amount)
 
     if not Config.systemMode.labor then
-
         return false, 'the_system_is_turned_off'
     end
 
     if not tonumber(playerId) or not ECO.meta[playerId] then
-
         return false, 'not_found_metadata'
     end
 
-    if not tonumber(amount) then
-
+    if not amount then
         return false, 'not_valid_amount'
     end
 
@@ -39,7 +33,6 @@ function setLabor(playerId, amount)
     ECO.meta[playerId].labor.time = os.time()
 
     syncRequest(playerId)
-
     return true
 end
 
@@ -47,24 +40,19 @@ end
 --- @param amount number of labor points to be removed
 --- @return boolean success and, in case of an error, the reason as well
 function removeLabor(playerId, amount)
-
     if not Config.systemMode.labor then
-
         return false, 'the_system_is_turned_off'
     end
 
     if not tonumber(playerId) or not ECO.meta[playerId] then
-
         return false, 'not_found_metadata'
     end
 
     if not tonumber(amount) then
-
         return false, 'not_valid_amount'
     end
 
     if ECO.meta[playerId].labor.val < 1 then
-
         return false, 'has_already_reached_the_limit'
     end
 
@@ -72,7 +60,6 @@ function removeLabor(playerId, amount)
     ECO.meta[playerId].labor.time = os.time()
 
     syncRequest(playerId)
-
     return true
 end
 
@@ -80,24 +67,19 @@ end
 --- @param amount number of labor points to be added
 --- @return boolean success and, in case of an error, the reason as well
 function addLabor(playerId, amount)
-
     if not Config.systemMode.labor then
-
         return false, 'the_system_is_turned_off'
     end
 
     if not tonumber(playerId) or not ECO.meta[playerId] then
-
         return false, 'not_found_metadata'
     end
 
     if not tonumber(amount) then
-
         return false, 'not_valid_amount'
     end
 
     if ECO.meta[playerId].labor.val >= Config.laborLimit then
-
         return false, 'has_already_reached_the_limit'
     end
 
@@ -105,7 +87,6 @@ function addLabor(playerId, amount)
     ECO.meta[playerId].labor.time = os.time()
 
     syncRequest(playerId)
-
     return true
 end
 
@@ -114,38 +95,31 @@ end
 -----------------------
 
 function laborIncrease()
-
     SetTimeout(Config.laborIncreaseTime * 60000, function()
-
         local timeStamp = os.time()
 
         for playerId, meta in pairs(ECO.meta) do
+            if meta.labor then
+                meta.labor.time = timeStamp
 
-            meta.labor.time = timeStamp
-
-            if (meta.labor and meta.labor.val < Config.laborLimit) then
-
-                meta.labor.val = hf.rangeLimit(
-                    meta.labor.val + Config.laborIncrease,
-                    Config.laborLimit
-                )
-
-                TriggerClientEvent('e_core:sync', playerId, meta)
+                if (meta.labor.val < Config.laborLimit) then
+                    meta.labor.val = hf.rangeLimit(
+                            meta.labor.val + Config.laborIncrease,
+                            Config.laborLimit)
+                    TriggerClientEvent('e_core:sync', playerId, meta)
+                end
             end
         end
-
         laborIncrease()
     end)
 end
 
 if Config.systemMode.labor and
-
     tonumber(Config.laborIncreaseTime) and
     Config.laborIncreaseTime > 0 and
 
     tonumber(Config.laborIncrease) and
     Config.laborIncrease > 0 then
-
     laborIncrease()
 end
 
@@ -153,26 +127,28 @@ end
 --- OFFLINE LABOR
 -----------------
 function addOfflineLabor(playerId)
-
     if not Config.systemMode.labor then
-
         return false
     end
 
-    if type(Config.laborIncreaseOffline) ~= 'number' or Config.laborIncreaseOffline < 1 then
+    local laborIncreaseOffline = tonumber(Config.laborIncreaseOffline)
+    local laborIncreaseTime = tonumber(Config.laborIncreaseTime)
 
+    if not laborIncreaseTime or not laborIncreaseOffline or laborIncreaseOffline < 1 then
         return false
     end
 
-    local increaseTime = Config.laborIncreaseTime * 60
-    local elapsedTime = os.time() - ECO.meta[playerId].labor.time
+    local timeStamp = os.time()
+    local increaseTime = laborIncreaseTime * 60
+    local elapsedTime = timeStamp - ECO.meta[playerId].labor.time
 
     if elapsedTime < increaseTime then
         return 0
     end
 
     local multiplier = math.floor(elapsedTime / increaseTime)
-    local offlineLabor = Config.laborIncreaseOffline * multiplier
+    local offlineLabor = laborIncreaseOffline * multiplier
 
+    ECO.meta[playerId].labor.time = timeStamp
     ECO.meta[playerId].labor.val = hf.rangeLimit(ECO.meta[playerId].labor.val + offlineLabor, Config.laborLimit)
 end
